b84f17afdf63a179915bd355b6936773e9270749
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-05-28 20:48:12 +0700

    refactor position-matches

--- src/mastermind/code_maker.clj
+++ src/mastermind/code_maker.clj
@@ -1,5 +1,11 @@
 (ns mastermind.code-maker)
 
+(defn position-matches [code guess]
+  (count
+    (filter identity
+            (map #(= %1 %2) code guess))))
+
+
 (defn score [code guess]
-  [(count (filter identity (map #(if (= %1 %2) :pos) code guess)))]
+  [(position-matches code guess)]
   )
