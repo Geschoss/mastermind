bada0b4e96e5c16b5d44661a9d5cb39a5de78968
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-05-28 21:06:26 +0700

    improve test comments

--- test/mastermind/code_maker_test.clj
+++ test/mastermind/code_maker_test.clj
@@ -22,7 +22,14 @@
     (score [1 2 3 4] [1 2 3 4]) => [4 0])
   )
 (facts
-  "Scoring Value Matches"
+  "Scoring Value Matches. Matches that have the
+  right value, but are in the wrong position"
   (fact
     "one value match"
-    (score [1 2 3 4] [2 0 0 0]) => [0 1]))
+    (score [1 2 3 4] [2 0 0 0]) => [0 1]
+    (score [1 2 3 4] [2 3 0 0]) => [0 2]
+    (score [1 2 3 4] [2 3 1 0]) => [0 3]
+    (score [1 2 3 4] [4 3 2 1]) => [0 4]
+    (score [1 2 3 4] [2 3 4 1]) => [0 4]
+    )
+  )
