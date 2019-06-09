9c8d3b04900a3892efc402a9e53602edfbca288d
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-06-09 14:05:12 +0700

    a nasty problem. Confound 1. if there are duplicate colours in guess

--- src/mastermind/code_maker.clj
+++ src/mastermind/code_maker.clj
@@ -9,8 +9,19 @@
 (defn value-match [code guess]
   (count-true (map #(contains? (set code) %1) guess)))
 
+(defn count-of [value values]
+  (count (filter #(= value %) values)))
+
+(defn over-count [code guess]
+  (let [code-values (set code)]
+    (reduce + (filter pos?
+                      (map #(- (count-of % guess) (count-of % code))
+                           code-values)))
+    ))
+
 (defn score [code guess]
   (let [p (position-matches code guess)
-        v (value-match code guess)]
-    [p (- v p)])
+        v (value-match code guess)
+        o (over-count code guess)]
+    [p (- v p o)])
   )
