ec7259582c86cd21a2cfe853e7d07588f5443786
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-06-17 20:52:35 +0700

    Two step decoding works. Ugly

--- src/mastermind/code_breaker.clj
+++ src/mastermind/code_breaker.clj
@@ -20,11 +20,11 @@
   (loop [guess (inc-guess last-guess)]
     (if (= guess [0 0 0 0])
       :error
-      (let [past-guess (first past-guesses)]
-        (if (= (cm/score guess (first past-guess))
-               (second past-guess))
-          guess
-          (recur (inc-guess guess)))))))
+      (if (every? identity (for [past-guess past-guesses]
+                             (= (cm/score guess (first past-guess))
+                                (second past-guess))))
+        guess
+        (recur (inc-guess guess))))))
 
 (defn break-code [last-guess past-guesses]
   (if (nil? last-guess)
